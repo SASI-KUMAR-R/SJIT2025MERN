2258ec76f19a54a657e03a081fbc6af8a89808f4
SASIKUMAR R <rsasikumar670@gmail.com>
2025-02-17 20:11:00 +0530

    Day7 An with React

--- DAY-7/callback.js
+++ DAY-7/callback.js
@@ -1,6 +1,9 @@
-function sjit(){
-
+function sjit(value,funcall){
+    console.log(value);
+    funcall();
 }
 function callbacksjit(){
-    
+    console.log("Second");
 }
+
+sjit("first",callbacksjit)
