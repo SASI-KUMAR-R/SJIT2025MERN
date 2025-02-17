47fd2ebaaf553037f5043f69198f25d60d01d5bf
SASIKUMAR R <rsasikumar670@gmail.com>
2025-02-17 07:56:49 +0530

    Task

--- TASK/DAY-6/Function.js
+++ TASK/DAY-6/Function.js
@@ -23,3 +23,18 @@ function returnnamewithsome(name)
 }
 res = returnnamewithsome("SASIKUMAR")
 console.log(res);
+
+// ----------------// ----------------// ----------------// ----------------// ----------------// ----------------
+object1={
+    firstname:"St.Joseph's",
+    "lastname":"Institute of Technology",
+    "depts":["CSE","IT","ADS","ECE"],
+    "training":"MERN"
+}
+Object.entries(object1).forEach(([key,ele])=>{
+    console.log(key,ele);
+})
+
+// --------------------------------------------------
+let printhello = (name) => "Hello " + name;
+console.log(printhello("World"));
