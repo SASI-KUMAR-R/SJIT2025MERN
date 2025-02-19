742628a6e9cfe4d4009d62cfd853414ff2c77a67
SASIKUMAR R <rsasikumar670@gmail.com>
2025-02-19 13:14:24 +0530

    d9 fn

--- sjitfrontend/src/App.jsx
+++ sjitfrontend/src/App.jsx
@@ -1,12 +1,31 @@
-import Home from './Components/ClassComponents/FunctionComponents/Home'
+import Home from './Components/FunctionComponents/Home'
+import About from './Components/FunctionComponents/About'
+import Image from './Components/FunctionComponents/Image'
+import Signup from './Components/FunctionComponents/SignUp'
+// import Contact from './Components/FunctionComponents/Contact'
+import Navbar from './Components/FunctionComponents/Navbar'
+import Gallery from './Components/FunctionComponents/Gallery'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <h1>WELCOME TO REACT</h1>
-      <hr />
-      <Home> </Home>
-    </div>
+    <main>
+
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route>
+          <Route path='/' element={<Home properties="FIRST" sjit="SAI" />} />
+          <Route path='/about' element={<About/>} />
+          <Route path='/img' element={<Image/>} />
+          <Route path='/Gallery' element={<Gallery/>} />
+          <Route path='/signup' element={<Signup/>} />
+          
+        </Route>
+      </Routes>
+      </BrowserRouter>
+
+    </main>
   )
 }
 
---
+++ sjitfrontend/src/Components/FunctionComponents/Navbar.jsx
@@ -0,0 +1,15 @@
+import '../css/Navbar.css'
+const navbar = () => {
+    return (
+        <header>
+        <nav style={{listStyleType:"circle"}}>
+            <li>Home</li>
+            <li>About</li>
+            <li>Gallery</li>
+            <li>Education</li>
+            <li>Contact</li>
+        </nav>
+    </header>
+    )
+}
+export default navbar
