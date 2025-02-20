b52c347059e1df6e777cd74a9f17c3707ed0becf
SASIKUMAR R <rsasikumar670@gmail.com>
2025-02-20 14:10:19 +0530

    DEPOLY

--- sjitfrontend/src/Components/FunctionComponents/Image.jsx
+++ sjitfrontend/src/Components/FunctionComponents/Image.jsx
@@ -1,6 +1,7 @@
-import img from '/Users/delishsasir/MYFILES/DEVLOP_PACK/MERN_STACK/sjitfrontend/src/assets/download.png'
-function Image(){
-    return(
+import img from '../../assets/download.png';
+function Image() {
+    return (
         <img src={img} alt="" />
-    )
-}export default Image
+    );
+}
+export default Image;
